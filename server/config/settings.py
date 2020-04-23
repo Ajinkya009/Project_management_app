bd0fe6539a86c3a0665440ad619cd12b9ae3927b
AJINKYA BALASAHEB DESHMUKH <ajinkyadeshmukh9@gmail.com>
2020-04-23 15:30:13 +0530

    updated requirements

--- server/config/settings.py
+++ server/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ndol$53d-d$95mtjd+05y*nubnv#fhx1d%la&*u!a&l=&s=vnn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
